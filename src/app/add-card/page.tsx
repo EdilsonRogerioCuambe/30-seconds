091577dbbba8fb73d787befdc22d75b7cffc98a1
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2024-05-16 04:09:50 -0300

    update

--- src/app/add-card/page.tsx
+++ src/app/add-card/page.tsx
@@ -4,10 +4,12 @@ import toast from 'react-hot-toast'
 import axios from 'axios'
 
 export default function Page() {
+  const [loading, setLoading] = useState(false)
   const [frontWords, setFrontWords] = useState<string[]>(['', '', '', ''])
   const [backWords, setBackWords] = useState<string[]>(['', '', '', ''])
 
   const handleSubmit = async () => {
+    setLoading(true)
     try {
       await axios.post('/api/add-card', {
         front: frontWords,
@@ -20,6 +22,8 @@ export default function Page() {
     } catch (error) {
       console.error(error)
       toast.error('Erro ao adicionar card')
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -27,7 +31,7 @@ export default function Page() {
     <div className="max-w-md mx-auto my-8 p-4 border rounded shadow-lg">
       <h2 className="text-xl font-bold mb-4 text-center">Adicionar Card</h2>
       <div className="mb-4">
-        <label htmlFor="front" className="block mb-2 text-sm text-[#333333]">
+        <label htmlFor="front" className="block mb-2 text-sm">
           Frente
         </label>
         {frontWords.map((word, index) => (
@@ -41,12 +45,12 @@ export default function Page() {
               newWords[index] = e.target.value
               setFrontWords(newWords)
             }}
-            className="w-full p-2 mb-2 border rounded"
+            className="w-full p-2 mb-2 border rounded bg-[#333333]"
           />
         ))}
       </div>
       <div className="mb-4">
-        <label htmlFor="back" className="block mb-2 text-sm text-[#333333]">
+        <label htmlFor="back" className="block mb-2 text-sm">
           Verso
         </label>
         {backWords.map((word, index) => (
@@ -60,7 +64,7 @@ export default function Page() {
               newWords[index] = e.target.value
               setBackWords(newWords)
             }}
-            className="w-full p-2 mb-2 border rounded"
+            className="w-full p-2 mb-2 border rounded bg-[#333333]"
           />
         ))}
       </div>
@@ -68,9 +72,11 @@ export default function Page() {
         title="submit"
         type="submit"
         onClick={handleSubmit}
-        className="w-full p-2 bg-blue-500 text-white rounded"
+        className={`w-full p-2 bg-green-400 text-white rounded hover:bg-green-600 transition ${
+          loading && 'cursor-not-allowed'
+        }`}
       >
-        Adicionar
+        {loading ? 'Adicionando...' : 'Adicionar'}
       </button>
     </div>
   )
